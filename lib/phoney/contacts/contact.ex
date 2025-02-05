32b400a35922b8e12873337ea299bd185d5b722e
Justin Winistörfer <winistoerfer.justin@gmx.ch>
2025-02-05 08:47:25 +0100

    define new create function for api upsert + migration

--- lib/phoney/contacts/contact.ex
+++ lib/phoney/contacts/contact.ex
@@ -19,7 +19,47 @@ defmodule Phoney.Contacts.Contact do
     timestamps()
   end
 
+  identities do
+    identity :email, [:email]
+  end
+
   actions do
     defaults [:read, :create, :update, :destroy]
+
+    create :upsert_from_api do
+      argument :firstname, :string
+      argument :lastname, :string
+      argument :email, :string
+      argument :phone, :string
+
+      argument :address, :map do
+        argument :street, :string
+        argument :city, :string
+        argument :zipcode, :string
+        argument :country, :string
+      end
+
+      upsert? true
+      upsert_identity :email
+
+      change fn changeset, _context ->
+        address = Ash.Changeset.get_argument(changeset, :address)
+        formatted_address = "#{address.street}, #{address.city}, #{address.zipcode}, #{address.country}"
+        Ash.Changeset.change_attribute(changeset, :address, formatted_address)
+      end
+
+      change set_attribute(:first_name, arg(:firstname))
+      change set_attribute(:last_name, arg(:lastname))
+      change set_attribute(:phone_number, arg(:phone))
+      change set_attribute(:email, arg(:email))
+
+      accept [:first_name, :last_name, :email, :phone_number, :address]
+    end
+  end
+
+  policies do
+    policy action_type(:create) do
+      authorize_if always()
+    end
   end
 end
