dedcf8d5bc413750e03c87bca8d8c643c916c273
Justin Winistörfer <winistoerfer.justin@gmx.ch>
2025-02-05 10:53:55 +0100

    added a search function for the contacts

--- lib/phoney_web/live/contacts_live/index.ex
+++ lib/phoney_web/live/contacts_live/index.ex
@@ -11,29 +11,61 @@ defmodule PhoneyWeb.ContactsLive.Index do
       socket
       |> stream(:contacts, [])
       |> assign(:page, 1)
+      |> assign(:search_term, "")
       |> list_contacts()
     }
   end
 
   defp list_contacts(socket) do
     page = socket.assigns.page
+    search_term = socket.assigns[:search_term] || ""
 
-    total_contacts_query = Phoney.Contacts.Contact
-                    |> Phoney.Contacts.read!()
+    base_query = Phoney.Contacts.Contact
 
-    total_contacts = length(total_contacts_query)
+    query = if search_term != "" do
+      filter_contacts(base_query, search_term)
+    else
+      base_query
+    end
 
-    total_pages = div(total_contacts, 20) + rem(total_contacts, 20)
+    # Get total count from paginated results
+    contacts_page = query
+      |> Ash.Query.sort(last_name: :asc)
+      |> Phoney.Contacts.read!()
 
-    query = Phoney.Contacts.Contact
-                |> Ash.Query.page(limit: 20, offset: (page - 1) * 20)
+    total_contacts = contacts_page |> length()
+    total_pages = div(total_contacts, 20) + if(rem(total_contacts, 20) > 0, do: 1, else: 0)
 
-    contacts = Ash.Query.sort(query, [last_name: :asc])
-                |> Phoney.Contacts.read!()
+    # Get paginated results
+    paginated_contacts = query
+      |> Ash.Query.page(limit: 20, offset: (page - 1) * 20)
+      |> Ash.Query.sort(last_name: :asc)
+      |> Phoney.Contacts.read!()
+      |> Map.get(:results)
 
     socket
     |> assign(:total_pages, total_pages)
-    |> stream(:contacts, contacts.results)
+    |> stream(:contacts, paginated_contacts, reset: true)
+  end
+
+  defp filter_contacts(query, search_term) do
+    query
+    |> Ash.Query.filter(
+      or: [
+        {:first_name, contains: search_term},
+        {:last_name, contains: search_term},
+        {:phone_number, contains: search_term}
+      ]
+    )
+  end
+
+  def handle_event("search", %{"search" => search_term}, socket) do
+    {:noreply,
+      socket
+      |> assign(:search_term, search_term)
+      |> assign(:page, 1)
+      |> list_contacts()
+    }
   end
 
   def handle_event("previous-page", _params, socket) do
