6b4e6541d699e3a515b811c764980f5fd2bafa55
Justin Winistörfer <winistoerfer.justin@gmx.ch>
2025-02-05 11:19:57 +0100

    added overlay to show contact details

--- lib/phoney_web/components/contact_component.ex
+++ lib/phoney_web/components/contact_component.ex
@@ -6,7 +6,7 @@ defmodule PhoneyWeb.ContactComponent do
   def contact_list_item(assigns) do
     ~H"""
     <div>
-      <div class="flex items-center justify-center gap-x-2 bg-gray-100 px-4 py-6 rounded-lg">
+      <div class="flex items-center justify-center gap-x-2 bg-gray-100 px-4 py-6 rounded-lg cursor-pointer hover:bg-gray-300" phx-click="select-contact" phx-value-contact_id={@contact.id}>
         <div><%= @contact.first_name%></div>
         <div class="font-bold"><%= @contact.last_name%></div>
       </div>
